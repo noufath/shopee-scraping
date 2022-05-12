e61d4be6bafbf6062b3228f8857a3aaca3244fbe
noufath <danychristianto@gmail.com>
2022-05-12 14:50:14 +0700

    initial commit

---
+++ applogger.py
@@ -0,0 +1,68 @@
+import logging
+from logging import config, getLogger
+
+
+dictLogging = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default_formatter': {
+            'format': '[%(levelname)s:%(asctime)s] %(funcName)s - %(message)s'
+        },
+    },
+    'handlers': {
+        'stream_handler': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default_formatter',
+        },
+        'rotating_file_handler': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'default_formatter',
+            'filename': 'App.log',
+            'mode': 'a',
+            'maxBytes': 2*1024*1024,
+            'backupCount': 4
+        },
+    },
+    'loggers': {
+        'debug_log': {
+            'handlers': ['stream_handler', 'rotating_file_handler'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'info_log': {
+            'handlers': ['rotating_file_handler'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'error_log': {
+            'handlers': ['stream_handler', 'rotating_file_handler'],
+            'level': 'ERROR',
+            'propagate': False
+        },
+        'warning_log': {
+            'handlers': ['rotating_file_handler'],
+            'level': 'WARNING',
+            'propagate': False
+        },
+        'critical_log': {
+            'handlers': ['rotating_file_handler'],
+            'level': 'CRITICAL',
+            'propagate': False
+        }
+    }
+}
+
+def AppLoger(logger_name):
+    config.dictConfig(dictLogging)
+
+    Loger = getLogger(logger_name)
+
+    return Loger
+  
+
+    
+
+        
+
+    
